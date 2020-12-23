41ec74c54626dee3d68d84325877c7ebc51e3a05
jqp18 <>
2020-12-23 17:00:26 +0800

    Add concept of config file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import threading
+import secret
 from telegram_bot import start_telegrambot
 from webserver import start_webserver
 
-telegrambot_thread = threading.Thread(target=start_telegrambot)
+telegrambot_thread = threading.Thread(
+    target=start_telegrambot, args=[secret.BOT_TOKEN])
 webserver_thread = threading.Thread(target=start_webserver)
 
-telegrambot_thread.start()
-webserver_thread.start()
+if __name__ == "__main__":
+    telegrambot_thread.start()
+    webserver_thread.start()
