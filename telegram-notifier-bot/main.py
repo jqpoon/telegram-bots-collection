b89228a5255f425bd4cc2f05a3148b1756b53a9b
Jia Qi Poon <jqp18@ic.ac.uk>
2021-02-14 19:09:19 +0800

    Update README for notifier bot

--- telegram-notifier-bot/main.py
+++ telegram-notifier-bot/main.py
@@ -3,6 +3,13 @@ import atexit
 from telegramBot import telegramNotifierBot
 from messageListener import messageListener
 
+# Include these in your secret.py files:
+# MESSAGE_PORT    : int    Port to communicate between client and listener
+# BOT_TOKEN       : str    Obtained from telegram botfather)
+# PICKLE_FILEPATH : str    Stores list of users that have successfully authenticated )
+# AUTH_KEY        : bytes  Instantiate as such b'your-key-here'
+# PASSWORD        : str    User chosen password to authenticate on telegram
+
 botInstance = telegramNotifierBot()
 listenerInstance = messageListener()
 telegrambotThread = threading.Thread(target=botInstance.startTelegramBot)
