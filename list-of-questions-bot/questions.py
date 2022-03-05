e51f4dc52f5324faaf4c7c0496d129306e5626d5
jqpoon <poonjiaqi@gmail.com>
2022-03-05 10:51:31 +0100

    Add questions list

---
+++ list-of-questions-bot/questions.py
@@ -0,0 +1,115 @@
+# Some questions adapted from the following sources:
+# - https://www.nytimes.com/2015/01/09/style/no-37-big-wedding-or-small.html
+# - https://github.com/jonathan-lph/wnrs
+
+questions_list = [
+    # Would you rather?
+    "Would you rather the world had no music or no wifi?",
+    "Would you rather be able to read minds or control them?",
+    "Would you rather marry someone you love or someone who loves you?",
+    "Would you rather know the date or cause of your death?",
+    "Would you rather have 8 kids or 8 dogs?",
+    "Would you rather flip a coin for a billion or get a guaranteed million?",
+    "Would you rather the person you love die first or die after you?",
+    "Would you rather have money or have love?",
+    "Would you rather love or be loved?",
+    "Would you rather pick the red pill or the blue pill?",
+    "Would you rather have happiness or wealth or health?",
+    "Would you rather have a boring relationship that is stable and last long, or one with very high highs but very low lows?",
+
+    # Would you?
+    "Would you immigrate to be with someone you love? Where?",
+    "Would you adopt kids?",
+    "Would you change your name?",
+    
+    # Do you?
+    "Do you regret anything from your past?",
+    "Do you miss anyone from your past?",
+    "Did you enjoy your childhood?",
+
+    # Have you?
+    "Have you ever liked someone and never told them?",
+    "Have you ever confessed to someone and got rejected?",
+    "Have you ever lost a friendship? What happened?",
+    "Have you done anything that you hope nobody finds out about?",
+    "Have you ever wished that you were someone else?",
+
+    # Are you?
+    "Are you a good friend? Why or why not?",
+    "Are any of your friends completely opposite to you or are most of them similar to you?",
+    "Are you close to your family?",
+
+    # If...
+    "If you could wake up tomorrow having gained any one quality or ability, what would it be?",
+    "If you had a time machine that only goes in one way, would you take it? If yes, would you go into the past or into the future?",
+    "If you knew that you had one year left to live, would you change anything about the way you're living? Why?",
+    "If someone wronged you terribly, would you rather forgive or forget or seek revenge?",
+    "If you had to share all your existing memories (and thoughts and feelings and everything) with someone, who would it be?",
+    "If you had to switch bodies with someone you know of the opposite gender for a week who would it be?",
+    "If your best friend of the opposite sex likes you, what would you do?",
+    "If you could change anything about the way you were raised, what would it be?",
+    "If a crystal ball could tell you the truth about yourself, your life, the future or anything else, what would you want to know?",
+    "If you were to die this evening with no opportunity to communicate with anyone, what would you most regret not having told someone? Why haven’t you told them yet?",
+    "If there was a library where each book is an alternate life where you made a different decision, which book would you choose?",
+    "If you had a wish what would you wish for? (Other than more wishes)",
+
+    # When
+    "When did you last cry and why?",
+    "When was the last time you went out by yourself / did something by yourself?",
+
+    # Who
+    "Who is the last person you cried in front of? Why?",
+    "Who is the last person who hurt you? Have you forgiven them?",
+    "Who do you look up to the most?",
+    "Who are you most comfortable around?",
+
+    # What?
+    "What do you want people's impression of you to be?",
+    "What is your greatest strength/weakness?",
+    "What is one compliment you wish you received more frequently?",
+    "What makes you happy?",
+    "What are you paranoid about?",
+    "What part of your body are you most uncomfortable with?",
+    "What do you admire the most about the other person?", #pair?
+    "What is something about the other person that you cannot understand?", #pair?
+    "What is your favourite quote?",
+    "What is something you hide about your personality?",
+    "What do you value most in a friendship?",
+    "What does friendship mean to you?",
+    "What is your most treasured memory?",
+    "What is your most terrible memory?",
+    "Is there something you've dreamed of doing for a long time? Why haven't you done it?",
+    "What in your life do you feel most grateful for?",
+    "What is one thing you wish you could change about yourself?",
+    "What kind of wedding would you want?",
+    "What would you want your obituary to say?",
+    "What is a big favor you have done for a friend? How about a big favor that a friend has done for you?",
+    "What quality do you value most in your friends?",
+    "What separates true friends from acquaintances?",
+    "What are the most common reasons for friendships to fall apart?",
+    "What is the greatest accomplishment of your life?",
+    "What is the most embarassing moment in your life?",
+    "What is the most sentimental item you own?",
+    "What is your MBTI?",
+    "What are your love languages?",
+    "What is your rice purity score?",
+    "What would constitute a perfect day for you?",
+    "What is one event that has made you who you are?",
+    "What do you do when you are stressed?",
+    "What do you do when you are sad?",
+    "What is one trait you appreciate in friends?",
+    "What is one type of person you don't like?",
+    "What were your childhood dreams?",
+    "What bad habits do you have?",
+    "What is the favourite part of your daily routine?",
+    "What does it mean when you are extremely quiet?",
+    "What do you want to do when you retire?",
+
+    # Misc
+    "Tell (someone in the room) something you like about them.",
+    "Tell (someone in the room) their greatest weakness.",
+    "Describe your childhood in one word.",
+    "Describe your ideal type."
+]
+
+print(len(questions_list))
